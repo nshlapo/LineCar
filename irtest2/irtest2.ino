be497a507e508ef904c2cfd691af79137bb3ffd0
Annabel Consilvio <annabel@students.olin.edu>
2015-10-02 12:27:21 -0400

    Update irtest2.ino

--- irtest2/irtest2.ino
+++ irtest2/irtest2.ino
@@ -1,1 +1,28 @@
+int sensorPin1 = A0; //the input pin for our IR sensor
+int sensorPin2 = A1;
+long sensorValue1 = 0; //initializing sensor value
+long sensorValue2 =0; //initializing sensor value
 
+
+void setup() {
+  Serial.begin(115200);  //initalize serial monitor
+}
+
+
+void loop() {
+  sensorValue1 = analogRead(sensorPin1);  //set sensorValue to current value of analog pin
+  sensorValue2 = analogRead(sensorPin2);  //set sensorValue to current value of analog pin
+  Serial.print(sensorValue2);
+  Serial.print(", ");
+  Serial.println(sensorValue1);  //print sensor value
+  delay(1000);  //wait 1.5 second before printing next value
+  
+  if (sensorValue1 >=  600)
+{
+  Serial.println("Move left motor");
+}
+  if (sensorValue2 >= 600)
+  {
+    Serial.println("Move right motor");
+  }
+} 
